8745da029f877f9221c849fdea109fcb334d8d56
Cesar Zulueta <cesarzulueta12@gmail.com>
2021-07-04 17:28:19 -0700

    built basic backend with server component models folder routes folder with todo component and todoRouter component

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const app = express() 
+const morgan = require("morgan")
+const mongoose = require("mongoose")
+
+app.use(express.json())
+app.use(morgan("dev"))
+
+mongoose.connect("mongodb://localhost:/27017/todoDB",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    },
+    () => ("connected to the database")
+)
+app.use("/todos", require("./routes/todoRouter.js"))
+app.use("/users", require("./routes/userRouter.js"))
+
+app.use((err, req, res, next) => {
+    return res.status({ errMsg: err.message})
+})
+
+app.listen(9000, () => {
+    console.log("running on port 9000")
+})
